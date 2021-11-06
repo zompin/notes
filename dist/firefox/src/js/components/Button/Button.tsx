d020b58d3c7dfc5de9314c2ce030906394f6fc7a
Ilya Altukhov <zompin@ya.ru>
2021-11-07 00:47:07 +0300

    ignore fix

--- dist/firefox/src/js/components/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React, {ComponentProps, forwardRef, Ref} from 'react'
-import cs from 'classnames'
-import * as style from './Button.module.css'
-
-export default forwardRef(function Button(props: ComponentProps<'button'> & { secondary?: boolean }, ref: Ref<HTMLButtonElement>) {
-  return (
-    <button {...props} className={cs(props.className, style.button, { [style.secondary]: props.secondary })} ref={ref} />
-  )
-})
--- dist/firefox/src/js/utils.ts
+++
@@ -1,6 +0,0 @@
-import { browser } from 'webextension-polyfill-ts';
-import { NAMES } from './constants';
-
-export const getLocaleMessage = (name: string, content?: string | string[]) => (
-  browser.i18n.getMessage(name, content)
-);
